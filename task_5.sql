711a0b118faac9690d7348f6bdd682a31faf1888
Sunmibare Idowu <sunmijewel@gmail.com>
2025-02-10 14:28:02 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 sql = "INSERT INTO customer (customer_id, customer_name, email, address) VALUES (%s, %s)"
-val = ( 1, "Cole Baidoo", "cbaidoo@sandtech.com", 123 Happiness Ave)
+val = ( 1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
 mycursor.execute(sql, val)
 mydb.commit()  # Commit the changes
