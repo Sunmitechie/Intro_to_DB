8f9b57ee060965986db5aa12124a2bd2b586583d
Sunmibare Idowu <sunmijewel@gmail.com>
2025-02-10 14:50:09 +0100

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,6 @@
 sql = "INSERT INTO customer (customer_id, customer_name, email, address) VALUES (%s, %s)"
-val = (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),  
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),  
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.'); 
+val = (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),  
+(3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),  
+(4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave."); 
 mycursor.execute(sql, val)
 mydb.commit()  # Commit the changes
