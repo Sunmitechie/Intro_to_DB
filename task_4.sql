d52ec1b34e1d5e75079d34d671104789f9d83c5a
Sunmibare Idowu <sunmijewel@gmail.com>
2025-02-10 13:34:23 +0100

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+import mysql connector
+
+mydb=mysql.connector.cursor(
+    host="loclhost",
+    user="jewel",
+    password="Jahdom1st!336791",
+    databases="alx_books_store"
+)
+
+mycursor=mydb.cursor()
+
+USE alx_book_store;
+SHOW TABLES;
+SHOW COLUMNS FROM Books;
