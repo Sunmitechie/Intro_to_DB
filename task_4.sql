162e364c9965dde50bc969dd6d63c883bc50a57d
Sunmibare Idowu <sunmijewel@gmail.com>
2025-02-10 13:57:11 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
 USE alx_book_store;
 FROM information_schema.COLUMNS
-WHERE SCHEMA TABLE_NAME = 'alx_book_store'
+WHERE TABLE_SCHEMA = 'alx_book_store'
 AND TABLE_NAME = 'Books'
 SELECT 
     COLUMN_NAME
